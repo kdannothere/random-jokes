d202cb31aa20fb37b5a76dd4410ae8e89d122d12
kdan <bohdanzhyhor@gmail.com>
2023-05-04 21:48:19 +0200

    small fixes

--- app/src/main/java/com/kdan/randomjokes/presentation/viewmodel/JokeViewModel.kt
+++ app/src/main/java/com/kdan/randomjokes/presentation/viewmodel/JokeViewModel.kt
@@ -1,16 +1,16 @@
 package com.kdan.randomjokes.presentation.viewmodel
 
-import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kdan.randomjokes.data.Joke
 import com.kdan.randomjokes.data.JokeApi
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-class JokeViewModel: ViewModel() {
+class JokeViewModel : ViewModel() {
 
     private val _jokes = MutableStateFlow(listOf<Joke>())
     val jokes = _jokes.asStateFlow()
@@ -22,25 +22,33 @@ class JokeViewModel: ViewModel() {
     }
 
     fun getTenJokes() {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
+            // clear list of jokes to show spinner
+            _jokes.value = emptyList()
+            // load new list of jokes
             try {
                 _jokes.value = JokeApi.retrofitService.getTenJokes()
-                Log.d("kdan_log", "getTenJokes()")
             } catch (e: Exception) {
-                _jokes.value = emptyList()
-                Log.d("kdan_log", "2 - ${e.message}")
+                _jokes.value += Joke(
+                    setup = "Please check your internet connection.",
+                    punchline = "If it`s OK then web-site is not available, sorry."
+                )
             }
         }
     }
 
     fun getOneJoke() {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
+            // clear list of jokes to show spinner
+            _jokes.value = emptyList()
+            // load new list of jokes
             try {
                 _jokes.value = listOf(JokeApi.retrofitService.getOneJoke())
-                Log.d("kdan_log", "getOneJoke()")
             } catch (e: Exception) {
-                _jokes.value = emptyList()
-                Log.d("kdan_log", "4 - ${e.message}")
+                _jokes.value += Joke(
+                    setup = "Please check your internet connection.",
+                    punchline = "If it`s OK then web-site is not available, sorry."
+                )
             }
         }
     }
